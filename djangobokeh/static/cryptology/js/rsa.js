7c741e51387e60d1da982503c5ee95f5bf3d3d95
Alex <pozden.sasha@gmail.com>
2020-12-14 19:25:20 +0200

    add checkbox change function

--- djangobokeh/static/cryptology/js/rsa.js
+++ djangobokeh/static/cryptology/js/rsa.js
@@ -4,6 +4,28 @@ const public_key = document.getElementById('public-key');
 const private_key = document.getElementById('private-key');
 const mod = document.getElementById('mod')
 
+const choose = document.getElementById('choose');
+
+function deOrEncode() {
+    if (choose.checked == true) {
+        public_key.required = false;
+        public_key.readOnly = true;
+        public_key.style.backgroundColor = '#C0C0C0';
+        private_key.required = true;
+        private_key.readOnly = false;
+        private_key.style.backgroundColor = 'inherit';
+    } else {
+
+        private_key.required = false;
+        private_key.readOnly = true;
+        private_key.style.backgroundColor = '#C0C0C0';
+        public_key.required = true;
+        public_key.readOnly = false;
+        public_key.style.backgroundColor = 'inherit';
+    }
+}
+
+
 function generateKeys() {
     fetch(uri)
         .then(response => response.json())
