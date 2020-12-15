6d222a786adc4f0fcc601d7cb69246dbbbde69b2
Alex <pozden.sasha@gmail.com>
2020-12-16 01:20:06 +0200

    modify-elgama.html

--- djangobokeh/outfunc/elgamal.py
+++ djangobokeh/outfunc/elgamal.py
@@ -6,41 +6,28 @@ import sys
 from random import randint
 import hashlib
 
+
+
 bits=60
-msg="Hello"
 
-if (len(sys.argv)>1):
-        msg=str(sys.argv[1])
-if (len(sys.argv)>2):
-        bits=int(sys.argv[2])
+def generate_keys():
+    p = Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
+    g = randint(1,p)
+    x = randint(1, p-1)
+    y = pow(g,x,p)
+    return {'publicKey': {'p':p,'g':g,'y':y},'privateKey':x}
 
-p = Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
-g=2
+def signature(msg,p,g,x):
+    k = Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
+    D = int.from_bytes(hashlib.sha256(msg.encode()).digest(),byteorder='big' )
+    e_1=(libnum.invmod(e, p-1))  
+    S_1=pow(g,k, p)
+    S_2=((D-x*S_1)*e_1) % (p-1)
+    return {"signedMessage":{'s1':S_1,:'s2':S_2}}
 
-s= randint(0, p-1)
-v = pow(g,s,p)
-
-e= Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
-# e_1=(gmpy2.invert(e, p-1))
-e_1=(libnum.invmod(e, p-1))
-#D=  bytes_to_long(msg.encode('utf-8')) // if you want direct signing
-
-D = int.from_bytes(hashlib.sha256(msg.encode()).digest(),byteorder='big' )
-
-S_1=pow(g,e, p)
-S_2=((D-s*S_1)*e_1) % (p-1)
-
-v_1 = (pow(v,S_1,p)*pow(S_1,S_2,p))%p
-v_2 = pow(g,D,p)
-
-print ("Message: %s " % msg)
-print ("g: %s" % g)
-print ("p: %s" % p)
-print ("\nv: %s" % v)
-print ("e: %s" % e)
-print ("\ns: %s" % s)
-
-print ("\nS_1= %s" % S_1)
-print ("S_2=%s" % S_2)
-print ("\nV_1=%s" % v_1)
-print ("v_2=%s" % v_2)
+def legal_check(msg,p,g,y,S_1,S_2):
+    v_1 = (pow(y,S_1,p)*pow(S_1,S_2,p))%p
+    v_2 = pow(g,D,p)
+    print(v_1)
+    print(v_2)
+    return v_1 == v_2
